f7274975a229e6d695afae5076f96bf17de07b3c
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-11-20 16:41:41 -0500

    passing tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusApp::Application.config.secret_token = '8d75096e35eced435d9e0b5a1ed3b56204b88d7fa8456be96613499abe4627f3106a71641519b005019a3c77a5680960d9a1beb0521022994b579407c6934560'
